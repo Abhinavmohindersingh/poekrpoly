315f6fc02f127b4b542970dabe2d8b0f211da667
BigSlickGames <bigslickgames@gmail.com>
2025-10-26 20:20:22 +1000

    Updated main.tsx

--- main.tsx
+++ main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AuthGate from "./components/AuthGate"; // new wrapper
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthGate />
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
